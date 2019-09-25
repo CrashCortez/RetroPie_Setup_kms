ba7da761e99781a62f9f13ff4459f78c0905cf91
CrashCortez <46716213+CrashCortez@users.noreply.github.com>
2019-09-25 10:22:53 -0700

    Update dolphin.sh

--- scriptmodules/emulators/dolphin.sh
+++ scriptmodules/emulators/dolphin.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # This file is part of The RetroPie Project
-#
+
 # The RetroPie Project is the legal property of its developers, whose names are
 # too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
 #
@@ -11,17 +11,22 @@
 
 rp_module_id="dolphin"
 rp_module_desc="Gamecube/Wii emulator Dolphin"
-rp_module_help="ROM Extensions: .iso\n\nCopy your gamecube roms to $romdir/gamecube and Wii roms to $romdir/wii"
+rp_module_help="ROM Extensions: .gcm .iso .wbfs .ciso .gcz\n\nCopy your gamecube roms to $romdir/gc and Wii roms to $romdir/wii"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/dolphin-emu/dolphin/master/license.txt"
 rp_module_section="exp"
-rp_module_flags="!arm"
+rp_module_flags=""
 
 function depends_dolphin() {
-    local depends=(cmake pkg-config libao-dev libasound2-dev libavcodec-dev libavformat-dev libbluetooth-dev libenet-dev libgtk2.0-dev liblzo2-dev libminiupnpc-dev libopenal-dev libpulse-dev libreadline-dev libsfml-dev libsoil-dev libsoundtouch-dev libswscale-dev libusb-1.0-0-dev libwxbase3.0-dev libwxgtk3.0-dev libxext-dev libxrandr-dev portaudio19-dev zlib1g-dev libudev-dev libevdev-dev libmbedtls-dev libcurl4-openssl-dev libegl1-mesa-dev)
+    local depends=(cmake pkg-config libao-dev libasound2-dev libavcodec-dev libavformat-dev libbluetooth-dev libenet-dev libgtk2.0-dev liblzo2-dev libminiupnpc-dev libopenal-dev libpulse-dev libreadline-dev libsfml-dev libsoil-dev libsoundtouch-dev libswscale-dev libusb-1.0-0-dev libwxbase3.0-dev libwxgtk3.0-dev libxext-dev libxrandr-dev portaudio19-dev zlib1g-dev libudev-dev libevdev-dev libmbedtls-dev libgtk2.0-dev
+libgtk-3-dev libcurl4-openssl-dev libegl1-mesa-dev qtbase5-private-dev)
     getDepends "${depends[@]}"
 }
 
 function sources_dolphin() {
+    local branch="master"
+    # current HEAD of dolphin doesn't build on Ubuntu 16.04 (with gcc 5.4)
+    compareVersions $__gcc_version lt 6.0.0 && branch="5.0"
+
     gitPullOrClone "$md_build" https://github.com/dolphin-emu/dolphin.git
 }
 
@@ -29,8 +34,9 @@ function build_dolphin() {
     mkdir build
     cd build
     cmake .. -DCMAKE_INSTALL_PREFIX="$md_inst"
-    make clean
+    #make clean
     make
+    md_ret_require="$md_build/build/Binaries/dolphin-emu"
 }
 
 function install_dolphin() {
@@ -50,7 +56,7 @@ function configure_dolphin() {
 [Display]
 FullscreenResolution = Auto
 Fullscreen = True
-_EOF_
+EOF
         chown -R $user:$user "$md_conf_root/gc/Config"
     fi
 
